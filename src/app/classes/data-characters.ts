740d4c2a43d50210c08963097fd0d05802186cdc
Mojo Major <info@redroostermobile.com>
2017-04-13 14:06:19 +0200

    routing, stati data, their types and rendering it all

---
+++ src/app/classes/data-characters.ts
@@ -0,0 +1,24 @@
+import { Character } from './character'
+
+export const CHARACTERS: Character[] = [
+    {
+        "name": "C-3PO",
+        "height": "167",
+        "mass": "75",
+        "hair_color": "n/a",
+        "skin_color": "gold",
+        "eye_color": "yellow",
+        "birth_year": "112BBY",
+        "gender": "n/a"
+    },
+    {
+        "name": "R2-D2",
+        "height": "96",
+        "mass": "32",
+        "hair_color": "n/a",
+        "skin_color": "white, blue",
+        "eye_color": "red",
+        "birth_year": "33BBY",
+        "gender": "n/a"
+    }
+];
