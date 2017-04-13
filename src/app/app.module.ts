c6a10e2c63b21c9ad8f0121fa964ac834d613fdc
Mojo Major <info@redroostermobile.com>
2017-04-13 14:45:44 +0200

    extracte movielist into own module. and added routing module with fallback

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,31 @@ import { HttpModule } from '@angular/http';
 
 import { RouterModule } from '@angular/router';
 
+
+import { MoviesModule } from './modules/movies/movies.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MoviesListComponent } from './components/movies-list/movies-list.component';
+
 import { CharactersListComponent } from './components/characters-list/characters-list.component';
-import { MovieComponent } from './components/movie/movie.component';
+
 import { CharacterComponent } from './components/character/character.component';
+import { PageNotFoundComponent} from './components/page-not-found.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesListComponent,
     CharactersListComponent,
-    MovieComponent,
-    CharacterComponent
+    CharacterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot([
-      {
-        path: 'movies', component: MoviesListComponent
-      },
-      {
-        path: 'characters', component: CharactersListComponent
-      }
-    ])
+    MoviesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
